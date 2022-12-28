5c5a3c4b711cc2402144c71b20de10fe06e9336d
yesyell <yelin5262@gmail.com>
2022-12-28 17:33:56 +0900

    정규표현식

--- CatStaGram/RegisterViewController.swift
+++ CatStaGram/RegisterViewController.swift
@@ -65,3 +65,22 @@ class RegisterViewController: UIViewController {
         }
     }
 }
+
+// 정규표현식
+extension String {
+    // 대문자, 소문자, 특수문자, 숫자 8자 이상일 떄, -> True
+    func isValidPassword() -> Bool {
+        let regularExpression = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}"
+        let passwordValidation = NSPredicate.init(format: "SELF MATCHES %@", regularExpression)
+        
+        return passwordValidation.evaluate(with: self)
+    }
+    
+    // @ 2글자
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluate(with: self)
+    }
+}
