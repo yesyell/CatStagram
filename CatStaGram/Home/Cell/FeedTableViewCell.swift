ee924c7a9c72b91bfdc02f80c251694453013e06
yesyell <yelin5262@gmail.com>
2023-03-03 00:54:45 +0900

    TableView

---
+++ CatStaGram/Home/Cell/FeedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.swift
+//  CatStaGram
+//
+//  Created by 김예린 on 2023/03/03.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CatStaGram/Home/HomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewController.swift
+//  CatStaGram
+//
+//  Created by 김예린 on 2023/03/03.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
